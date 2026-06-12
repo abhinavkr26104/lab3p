27e0da7a189a0357b34d4b2dee014d732dd98f81
Abhinav Kumar Singh <152812718+abhinavkr26104@users.noreply.github.com>
2026-06-12 20:41:49 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,4 +1,6 @@
 package com.example;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -11,11 +13,14 @@ public class App {
         options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--disable-gpu");
 
         WebDriver driver = new ChromeDriver(options);
 
-        driver.get("https://www.google.com");
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
 
         System.out.println(driver.getTitle());
 
