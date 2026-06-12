cca915bb3a670ee82f0c615042bd40b393d676b5
Abhinav Kumar Singh <152812718+abhinavkr26104@users.noreply.github.com>
2026-06-12 20:04:01 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -8,7 +8,10 @@ public class App {
         ChromeOptions options = new ChromeOptions();
 
         options.addArguments("--headless=new");
-       
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+
         WebDriver driver = new ChromeDriver(options);
 
         driver.get("https://www.google.com");
